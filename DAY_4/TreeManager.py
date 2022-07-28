9b4805d4f6d2a3e2264cfe516ea73901d55851e5
wwotex <wwojtek55@gmail.com>
2022-07-28 18:35:55 +0200

    tree manager updated for it to scale the canvas with the tree size

--- DAY_4/TreeManager.py
+++ DAY_4/TreeManager.py
@@ -1,29 +1,36 @@
 class TreeNode:
-    def __init__(self, x):
+    def __init__(self, x, height):
         self.val = x
         self.left = None
         self.right = None
+        self.height = height
 
 
 class BST:
     def __init__(self, ls_tree):
-        # null = None
+        self.max_height = 0
+
+        if not ls_tree:
+            return
+        
         fifo = []
-        self.tree = TreeNode(ls_tree.pop(0))
+        self.tree = TreeNode(ls_tree.pop(0), 1)
         fifo.append(self.tree)
 
         while fifo:
             curr = fifo.pop(0)
-            
+            if curr.height > self.max_height:
+                self.max_height = curr.height
+
             temp = None
             if ls_tree:
                 temp = ls_tree.pop(0)
                 if temp is not None:
-                    curr.left = TreeNode(temp)
+                    curr.left = TreeNode(temp, curr.height + 1)
             if ls_tree:
                 temp = ls_tree.pop(0)
                 if temp is not None:
-                    curr.right = TreeNode(temp)
+                    curr.right = TreeNode(temp, curr.height + 1)
             
             if curr.left: fifo.append(curr.left)
             if curr.right: fifo.append(curr.right)
@@ -31,19 +38,20 @@ class BST:
         print('created BST')
 
     def print(self):
-        init_canvas_width = 60
-        init_canvas_height = 20
-        root_position = 30
+        init_canvas_width = 2 **(self.max_height + 1)+20
+        init_canvas_height = self.max_height + 2
+        root_position = 2**(self.max_height)+10
+        init_spread = 2**(self.max_height - 1)
         self.tree_canvas = [[' ' for x in range(init_canvas_width)] for y in range(init_canvas_height)]
 
-        self._drawNode(self.tree, 0, root_position, 8)
+        self._drawNode(self.tree, 0, root_position, init_spread)
         for el in self.tree_canvas:
             print(''.join(el))
 
     def _drawNode(self, node, height, position, spread):
         self.tree_canvas[height][position] = str(node.val)
-        if node.left: self._drawNode(node.left, height+1, position-spread+1, int(spread/2))
-        if node.right: self._drawNode(node.right, height+1, position+spread-1, int(spread/2))
+        if node.left: self._drawNode(node.left, height+1, position-spread, int(spread/2))
+        if node.right: self._drawNode(node.right, height+1, position+spread, int(spread/2))
 
 
 
--- DAY_4/lowestCommonAncestorOfBST.py
+++ DAY_4/lowestCommonAncestorOfBST.py
@@ -8,6 +8,7 @@ class Solution:
 sol = Solution()
 # BST testing module
 null = None
-ls_tree = [6,2,8,0,4,7,9,null,null,3,5]
+ls_tree = [6,2,8,0,4,7,9,null,null,3,5,null,null,null,20]
+# ls_tree = list(range(20))
 bst = BST(ls_tree)
 bst.print()
