bd3de9fc0c2b95bdf07b1275ef4c289510765661
wwotex <wwojtek55@gmail.com>
2022-08-29 23:05:30 +0200

    finished task scheduler and moved

--- DAY_11/taskScheduler.py
+++ DAY_11/taskScheduler.py
@@ -10,21 +10,22 @@ class Solution:
         Q = []
         cycle = 0
         while task_heap or Q:
+            if Q and Q[0][1] == cycle:
+                temp = Q.pop(0)
+                heapq.heappush(task_heap, temp[0])
             if task_heap:
                 task = heapq.heappop(task_heap)
                 task += 1
                 if task != 0:
-                    Q.append((task, cycle + n))
+                    Q.append((task, cycle + n + 1))
             cycle += 1
-            if Q[-1][1] == cycle:
-                temp = Q.pop()
-                heapq.heappush(task_heap, temp[0])
+            
         return cycle
                 
 
 
 sol = Solution()
 
-tasks = ["A","A","A","B","B","B", "C","C","C", "D", "D", "E"]
+tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"]
 n = 2
 print(sol.leastInterval(tasks, n))
