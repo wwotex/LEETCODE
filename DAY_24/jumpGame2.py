b8191b2e3743169f438e2d76f5d710b00b87443e
wwotex <wwojtek55@gmail.com>
2022-09-21 02:52:13 +0200

    solutions dp

---
+++ DAY_24/jumpGame2.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        steps = [-1 for el in nums]
+        best_stamina[]
+        for i in range(0, len(nums)):
+            if nums[i] > best_stamina:
+                best_stamina = nums
+
+        print(steps)
+        return steps[-1]        
+
+sol = Solution()
+nums = [2,0,2,3,4]
+print(sol.jump(nums))
