ec8613cecf2bd6c8d10dc56a777eb921ad971b58
wwotex <wwojtek55@gmail.com>
2022-09-26 13:56:57 +0200

    tinkering with subtree of another tree . no success

--- DAY_29/subtreeOfAnotherTree.py
+++ DAY_29/subtreeOfAnotherTree.py
@@ -3,11 +3,41 @@
 from typing import Optional
 from TreeManager import BST, TreeNode
 
-class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:    
-        def merkle(node):
-            return f'^{node.val}.{merkle(node.left)} {merkle(node.right)}' if node else '#'
-        return merkle(subRoot) in merkle(root)
+# class Solution:
+#     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:    
+#         def isSameOrContains(node, comp):
+#             print(f'\n\nrec: {node.val if node else None}, {comp.val if comp else None}')
+#             if not node and not comp: 
+#                 print('True, False')
+#                 return True, False
+#             if not comp:
+#                 print('no comp->keep going')
+#                 return isSameOrContains(node, subRoot)
+#             if not node:
+#                 print('False, False')
+#                 return False, False
+            
+#             if node.val != comp.val:
+#                 print('wrong val')
+#                 isLeftSame, leftContains = isSameOrContains(node.left, subRoot)
+#                 isRightSame, rightContains = isSameOrContains(node.right, subRoot)
+#                 print(f'False, {(leftContains or rightContains)} for -> {node.val if node else None}, {comp.val if comp else None}')
+#                 return False, (leftContains or rightContains)
+#             else:
+#                 print('good val')
+#                 isLeftSame, leftContains = isSameOrContains(node.left, comp.left)
+#                 isRightSame, rightContains = isSameOrContains(node.right, comp.right)
+#                 print(f'{(isLeftSame and isRightSame)}, {(leftContains or rightContains)} for -> {node.val if node else None}, {comp.val if comp else None}')
+#                 return (isLeftSame and isRightSame), (leftContains or rightContains or isLeftSame or isRightSame)
+#         isSame, contains = isSameOrContains(root, subRoot)
+#         return contains
+
+
+# class Solution:
+#     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:    
+#         def merkle(node):
+#             return f'^{node.val}.{merkle(node.left)} {merkle(node.right)}' if node else '#'
+#         return merkle(subRoot) in merkle(root)
 
 sol = Solution()
 null = None
