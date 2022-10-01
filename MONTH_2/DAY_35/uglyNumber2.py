26e7237f52a13e29e78b5b75066ff310acda1c72
wwotex <wwojtek55@gmail.com>
2022-10-01 23:28:27 +0100

    added solution

---
+++ MONTH_2/DAY_35/uglyNumber2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        count = 1
+        ugly = [1]
+        i2, i3, i5 = 0, 0, 0
+        while count < n:
+            u2, u3, u5 = 2*ugly[i2], 3*ugly[i3], 5*ugly[i5]
+            umin = min(u2, u3, u5)
+            if umin == u2:
+                i2 += 1
+            if umin == u3:
+                i3 += 1
+            if umin == u5:
+                i5 += 1
+            ugly.append(umin)
+            count += 1
+        
+        return ugly[-1]
+
+
+sol = Solution()
+n = 30
+sol.nthUglyNumber(n)
